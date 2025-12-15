a57c06fbbda52b31f4279d4e9c31fe03b8ca448f
N1sh <nishanth23082004@gmail.com>
2025-12-15 18:21:44 +0530

    done with useCase2

--- Snake&Ladder.py
+++ Snake&Ladder.py
@@ -1,3 +1,13 @@
+import random
 players = [0]
 Starting = 0
+
+win = False
+
+while not win:
+    print("Hit Enter to role the dice:")
+    input("")
+    dice_val = random.randint(1,6)
+    print(dice_val)
+
 
