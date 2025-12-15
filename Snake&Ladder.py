4314993b652d00bdab9e5b9b48b942c4b1782b89
N1sh <nishanth23082004@gmail.com>
2025-12-15 20:17:59 +0530

    done with use case 7

--- Snake&Ladder.py
+++ Snake&Ladder.py
@@ -1,16 +1,32 @@
 import random
 players = {
-    "player1":0,
+        0:0,
+        1:0
     
 }
+count={
+    0:0,
+    1:0
+}
+report_player1={
+}
 
-report={
+report_player2={
 
 }
 
-current_player = "player1"
+players_report ={
+    0:report_player1,
+    1:report_player2
+}
 
-count = 0
+
+
+
+
+current_player = 0
+
+
 # Starting = 0
 
 win = False
@@ -23,11 +39,11 @@ option_name = {
 
 while not win:
     print("\n")
-    print("Hit Enter to role the dice:")
-    count += 1
+    print(f"Player {current_player} plz Hit Enter to role the dice:")
+    count[current_player] += 1
     input("")
     dice_val = random.randint(1,6)
-    print(dice_val)
+    print(f"dice val:{dice_val}")
 
     option = random.randint(0,2)
  
@@ -35,38 +51,50 @@ while not win:
     if option == 0:
         print(f"its {option_name[0]} ")
 
-        report[count] =f"Position:  {players[current_player]}-->{players[current_player]}"
+        players_report[current_player][count[current_player]] =f"Position:  {players[current_player]}-->{players[current_player]}"
+        print(f"\n player postion of palyer-{current_player}: {players[current_player]} :at count {count[current_player]}")  
+        current_player = 1 - current_player
 
 
     elif option == 1:
         print(f"its {option_name[1]} ")
-        if players[current_player] + dice_val > 100:
-            pass
-        else:
+        if players[current_player] + dice_val <= 100:
             previous_posiction = players[current_player]
             players[current_player] += dice_val
-            report[count] = f"Position:  {previous_posiction}-->{players[current_player]}"
-
-
+            players_report[current_player][count[current_player]] = f"Position:  {previous_posiction}-->{players[current_player]}"
+            print(f"\n player postion of palyer-{current_player}: {players[current_player]} :at count {count[current_player]}")  
+           
 
             # checking the win
-        if players[current_player] ==100:
-            print("\n------Report---------\n")
-            for key,value in report.items():
+        if players[current_player] == 100:
+            print(f"\n------Report---------\n for player {current_player }")
+            for key,value in players_report[current_player].items():
                 print(f"{key}:   {value}")
             win =True
+        current_player = 1 - current_player
 
     else:
          print(f"its {option_name[2]} ")
          if players[current_player] - dice_val <= 0:
              previous_posiction = players[current_player]
              players[current_player] = 0
-             report[count] = f"Position:{previous_posiction}-->{players[current_player]}"
+             players_report[current_player][count[current_player]] = f"Position:  {previous_posiction}-->{players[current_player]}"
+             print(f"\n player postion of palyer-{current_player}: {players[current_player]} :at count {count[current_player]}")  
+             current_player = 1 - current_player
+         else:
+             
+             previous_posiction = players[current_player]
+             players[current_player] -= dice_val
+           
+             players_report[current_player][count[current_player]] = f"Position:  {previous_posiction}-->{players[current_player]}"
+             print(f"\n player postion of palyer-{current_player}: {players[current_player]} :at count {count[current_player]}")  
+             current_player = 1 - current_player
+
          
          
      
 
-    print(f"\n player postion {current_player}: {players[current_player]} :at {count}")     
+      
                      
           
         
